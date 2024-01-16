1601b7719ce1a0758cc8cfd806aa8c088ac34e7a
KimGyeongwon <ruddnjs6651@gmail.com>
2024-01-16 21:39:27 +0900

    greetings 완성

---
+++ src/app/(home)/greetings/GreetingClient.tsx
@@ -0,0 +1,146 @@
+"use client";
+import Image from "next/image";
+
+import IDPhoto from "@/assets/greetings/greeting-photo.png";
+import clip from "@/assets/greetings/photo-clip.svg";
+import band from "@/assets/greetings/band-removebg.png";
+import award1 from "@/assets/greetings/award1.png";
+import award2 from "@/assets/greetings/award2.png";
+import media1 from "@/assets/greetings/media1.png";
+import media2 from "@/assets/greetings/media2.png";
+import media3 from "@/assets/greetings/media3.png";
+import media4 from "@/assets/greetings/media4.png";
+import media5 from "@/assets/greetings/media5.png";
+
+const GreetingClient = () => {
+  const awards = [award1, award2];
+  const medias = [media1, media2, media3, media4, media5];
+  return (
+    <div className="relative z-30 flex flex-col items-center w-full p-64">
+      <div className="h-[750px] w-full flex gap-60 justify-between">
+        <div className="w-[425px] h-[640px] ml-14">
+          <div className="w-full h-[547px] relative mb-12 rounded-sm">
+            <Image src={IDPhoto} alt="IDphoto" fill={true} />
+            <Image
+              src={clip}
+              alt="clip"
+              width={238}
+              height={238}
+              className="absolute -bottom-[84px] -right-[84px]"
+            />
+          </div>
+          <p className="text-sm">
+            E-Mail : 이메일@gmail.com <br /> Kakaotalk : @카카오톡ID
+          </p>
+        </div>
+        <div className="w-1/2">
+          <h2 className="text-[40px] leading-[52px] font-medium mt-10 mb-[72px]">
+            <span className="relative text-6xl before:content-[''] before:block before:w-2 before:h-2 before:rounded-full before:bg-slate-300 before:absolute before: before:-top-5 before:left-1/2 before:transform before:-translate-y-1/2">
+              샤
+            </span>
+            <span className="relative text-6xl before:content-[''] before:block before:w-2 before:h-2 before:rounded-full before:bg-slate-300 before:absolute before: before:-top-5 before:left-1/2 before:transform before:-translate-y-1/2">
+              뜨
+            </span>
+            와{" "}
+            <span className="relative text-6xl before:content-[''] before:block before:w-2 before:h-2 before:rounded-full before:bg-slate-300 before:absolute before: before:-top-5 before:left-1/2 before:transform before:-translate-y-1/2">
+              함
+            </span>
+            <span className="relative text-6xl before:content-[''] before:block before:w-2 before:h-2 before:rounded-full before:bg-slate-300 before:absolute before: before:-top-5 before:left-1/2 before:transform before:-translate-y-1/2">
+              께
+            </span>
+            하세요
+          </h2>
+          <div className="w-full">
+            <h2 className="text-3xl font-bold mb-2 ml-1">CEO</h2>
+            <h2 className="text-5xl font-bold tracking-tighter mb-8">이주연</h2>
+            <p className="text-lg">
+              모던마스터스는 세계 1위의 고급 페인트 공급사로서 메탈릭 페인트,
+              부식효과 페인트, 건축 외부용 페인트, 테마 페인트, 블랙라이트
+              페인트, 글레이즈, 바니쉬, 크랙클 페인트, 건축용 기능성 페인트,
+              DIY, 건축용 도료, 장식용 페인트 등 수성 페인트 전문 업체입니다.
+              <br />
+              <br />
+              모던마스터스는 세계 1위의 고급 페인트 공급사로서 메탈릭 페인트,
+              부식효과 페인트, 건축 외부용 페인트, 테마 페인트, 블랙라이트
+              페인트, 글레이즈, 바니쉬, 크랙클 페인트, 건축용 기능성 페인트,
+              DIY, 건축용 도료, 장식용 페인트 등 수성 페인트 전문 업체입니다.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-[342px] bg-bgGray border border-[#e4e4e4] relative rounded-[4px] mb-32">
+        <Image
+          className="rotate-[41deg] absolute -top-4 -right-12 opacity-80"
+          src={band}
+          alt="band"
+          width={127}
+          height={57}
+        />
+        <div className="flex p-10 pr-28 justify-start items-center w-full h-full gap-60">
+          <div className="flex gap-9 justify-start items-start h-full">
+            <div className="bg-[#9BB5B5] w-[6px] h-full" />
+            <ul className="text-lg flex flex-col gap-2">
+              <li>1998 수채화 교육장 개설</li>
+              <li>2002 칼헤인즈 마블페인팅 수료</li>
+              <li>2009 미국 모던마스터즈 어플리케이터 수료</li>
+              <li>2010 미국 모던마스터즈 어플리케이터 수료</li>
+              <li>2010 미국 어메리칸클레이 어플리케이터 수료</li>
+              <li>2011 미국 모던마스터즈 어플리케이터 수료</li>
+              <li>2013 미국 모던마스터즈 어드밴스드 수료</li>
+            </ul>
+          </div>
+          <div className="flex gap-9 justify-start items-start h-full">
+            <div className="bg-[#9BB5B5] w-[6px] h-full" />
+            <ul className="text-lg flex flex-col gap-2">
+              <li>2013 스페셜페인팅 민간자격등록(2013-1086)</li>
+              <li>2018 호주 포터스페인트 디테일스킬 수료</li>
+              <li>2018 포페인팅 민간자격 명칭변경(2013-1086)</li>
+              <li>2021 피엘핀켈스테인 우드/마블 페인팅 수료</li>
+              <li>2023 독일 마이다스메탈 어플리케이터 수료</li>
+              <li>2023 이태리 노바컬러 어드밴스드클래스 수료</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-start w-full mb-24">
+        <h2 className="text-3xl font-bold mb-2">수료증</h2>
+        <p className="text-lg mb-6">
+          국내외 최고의 기관에서 교육 관정을 수료하였습니다.
+        </p>
+        <div className="flex gap-6">
+          {awards.map((award, index) => (
+            <Image
+              key={index}
+              src={award}
+              alt="award"
+              width={277}
+              height={195}
+              className="rounded-sm"
+            />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col justify-start w-full">
+        <h2 className="text-3xl font-bold mb-2">미디어 출연</h2>
+        <p className="text-lg mb-6">
+          국내 다양한 TV, 라디오, 유튜브에 프로그램에 출연하였습니다.
+        </p>
+        <div className="flex w-[768px] flex-wrap gap-3">
+          {medias.map((media, index) => (
+            <div className="relative w-[244px] h-[166px]" key={index}>
+              <Image
+                key={index}
+                src={media}
+                alt="media"
+                fill={true}
+                className="rounded-[4px]"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GreetingClient;
