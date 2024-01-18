32353b864c8de90fbaa5f6f6e03f50737d65b439
KimGyeongwon <ruddnjs6651@gmail.com>
2024-01-19 00:57:12 +0900

    애니메이션 추가

--- src/app/(home)/greetings/GreetingClient.tsx
+++ src/app/(home)/greetings/GreetingClient.tsx
@@ -12,12 +12,51 @@ import media3 from "@/assets/greetings/media3.png";
 import media4 from "@/assets/greetings/media4.png";
 import media5 from "@/assets/greetings/media5.png";
 
+import { motion } from "framer-motion";
+
 const GreetingClient = () => {
   const awards = [award1, award2];
   const medias = [media1, media2, media3, media4, media5];
+
+  const container = {
+    hidden: { opacity: 0, y: 100 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        duration: 0.5,
+        ease: "easeIn",
+        staggerChildren: 0.2,
+        delayChildren: 0.1,
+      },
+    },
+  };
+
+  const li = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { ease: "easeIn" } },
+  };
+
+  const hover = {
+    width: "300px", // 244px * 1.2
+    height: "200px", // 166px * 1.2
+    zIndex: 1,
+    transition: { duration: 0.2, ease: "easeInOut" },
+  };
+
   return (
     <div className="relative z-30 flex flex-col items-center w-full p-64">
-      <div className="h-[750px] w-full flex gap-60 justify-between">
+      <motion.div
+        className="h-[750px] w-full flex gap-60 justify-between"
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{
+          opacity: 1,
+          transition: { duration: 1, ease: "easeIn" },
+          y: 0,
+        }}
+        viewport={{ once: true }}
+      >
         <div className="w-[425px] h-[640px] ml-14">
           <div className="w-full h-[547px] relative mb-12 rounded-sm">
             <Image src={IDPhoto} alt="IDphoto" fill={true} />
@@ -67,8 +106,14 @@ const GreetingClient = () => {
             </p>
           </div>
         </div>
-      </div>
-      <div className="w-full h-[342px] bg-bgGray border border-[#e4e4e4] relative rounded-[4px] mb-32">
+      </motion.div>
+      <motion.div
+        className="w-full h-[342px] bg-bgGray border border-[#e4e4e4] relative rounded-[4px] mb-32"
+        initial="hidden"
+        whileInView="visible"
+        variants={container}
+        viewport={{ once: true }}
+      >
         <Image
           className="rotate-[41deg] absolute -top-4 -right-12 opacity-80"
           src={band}
@@ -80,54 +125,96 @@ const GreetingClient = () => {
           <div className="flex gap-9 justify-start items-start h-full">
             <div className="bg-[#9BB5B5] w-[6px] h-full" />
             <ul className="text-lg flex flex-col gap-2">
-              <li>1998 수채화 교육장 개설</li>
-              <li>2002 칼헤인즈 마블페인팅 수료</li>
-              <li>2009 미국 모던마스터즈 어플리케이터 수료</li>
-              <li>2010 미국 모던마스터즈 어플리케이터 수료</li>
-              <li>2010 미국 어메리칸클레이 어플리케이터 수료</li>
-              <li>2011 미국 모던마스터즈 어플리케이터 수료</li>
-              <li>2013 미국 모던마스터즈 어드밴스드 수료</li>
+              <motion.li variants={li}>1998 수채화 교육장 개설</motion.li>
+              <motion.li variants={li}>2002 칼헤인즈 마블페인팅 수료</motion.li>
+              <motion.li variants={li}>
+                2009 미국 모던마스터즈 어플리케이터 수료
+              </motion.li>
+              <motion.li variants={li}>
+                2010 미국 모던마스터즈 어플리케이터 수료
+              </motion.li>
+              <motion.li variants={li}>
+                2010 미국 어메리칸클레이 어플리케이터 수료
+              </motion.li>
+              <motion.li variants={li}>
+                2011 미국 모던마스터즈 어플리케이터 수료
+              </motion.li>
+              <motion.li variants={li}>
+                2013 미국 모던마스터즈 어드밴스드 수료
+              </motion.li>
             </ul>
           </div>
           <div className="flex gap-9 justify-start items-start h-full">
             <div className="bg-[#9BB5B5] w-[6px] h-full" />
             <ul className="text-lg flex flex-col gap-2">
-              <li>2013 스페셜페인팅 민간자격등록(2013-1086)</li>
-              <li>2018 호주 포터스페인트 디테일스킬 수료</li>
-              <li>2018 포페인팅 민간자격 명칭변경(2013-1086)</li>
-              <li>2021 피엘핀켈스테인 우드/마블 페인팅 수료</li>
-              <li>2023 독일 마이다스메탈 어플리케이터 수료</li>
-              <li>2023 이태리 노바컬러 어드밴스드클래스 수료</li>
+              <motion.li variants={li}>
+                2013 스페셜페인팅 민간자격등록(2013-1086)
+              </motion.li>
+              <motion.li variants={li}>
+                2018 호주 포터스페인트 디테일스킬 수료
+              </motion.li>
+              <motion.li variants={li}>
+                2018 포페인팅 민간자격 명칭변경(2013-1086)
+              </motion.li>
+              <motion.li variants={li}>
+                2021 피엘핀켈스테인 우드/마블 페인팅 수료
+              </motion.li>
+              <motion.li variants={li}>
+                2023 독일 마이다스메탈 어플리케이터 수료
+              </motion.li>
+              <motion.li variants={li}>
+                2023 이태리 노바컬러 어드밴스드클래스 수료
+              </motion.li>
             </ul>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col justify-start w-full mb-24">
+      </motion.div>
+      <motion.div
+        className="flex flex-col justify-start w-full mb-24"
+        initial="hidden"
+        whileInView="visible"
+        variants={container}
+      >
         <h2 className="text-3xl font-bold mb-2">수료증</h2>
         <p className="text-lg mb-6">
           국내외 최고의 기관에서 교육 관정을 수료하였습니다.
         </p>
-        <div className="flex gap-6">
+        <motion.div className="flex gap-6">
           {awards.map((award, index) => (
-            <Image
+            <motion.div
+              className="relative w-[277px] h-[195px]"
               key={index}
-              src={award}
-              alt="award"
-              width={277}
-              height={195}
-              className="rounded-sm"
-            />
+              variants={li}
+              whileHover={hover}
+            >
+              <Image
+                src={award}
+                alt="award"
+                fill={true}
+                className="rounded-sm"
+              />
+            </motion.div>
           ))}
-        </div>
-      </div>
-      <div className="flex flex-col justify-start w-full">
+        </motion.div>
+      </motion.div>
+      <motion.div
+        className="flex flex-col justify-start w-full"
+        initial="hidden"
+        whileInView="visible"
+        variants={container}
+      >
         <h2 className="text-3xl font-bold mb-2">미디어 출연</h2>
         <p className="text-lg mb-6">
           국내 다양한 TV, 라디오, 유튜브에 프로그램에 출연하였습니다.
         </p>
-        <div className="flex w-[768px] flex-wrap gap-3">
+        <div className="flex flex-wrap w-[820px] gap-3">
           {medias.map((media, index) => (
-            <div className="relative w-[244px] h-[166px]" key={index}>
+            <motion.div
+              className="relative w-[244px] h-[166px]"
+              key={index}
+              variants={li}
+              whileHover={hover}
+            >
               <Image
                 key={index}
                 src={media}
@@ -135,10 +222,10 @@ const GreetingClient = () => {
                 fill={true}
                 className="rounded-[4px]"
               />
-            </div>
+            </motion.div>
           ))}
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
