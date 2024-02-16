c54fd6325efd584afad5c8ba799d4f5b369da281
pelicanStd <pelican8118@gmail.com>
2024-02-16 18:37:37 +0900

    feat: 교육 생성

--- src/app/(education)/education/TopBanner.tsx
+++ src/app/(education)/education/TopBanner.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import Banner from "@/assets/education/img.png";
-import TopBannerImage02 from "@/assets/education/topbanner-02.jpg";
-import TopBannerImage01 from "@/assets/education/topbanner-01.jpg";
+import Banner from "@/assets/education/edubanner.jpg";
+// import TopBannerImage02 from "@/assets/education/topbanner-02.jpg";
+// import TopBannerImage01 from "@/assets/education/topbanner-01.jpg";
+
 export default function TopBanner() {
   return (
     <section>
-      <Image src={ Banner } alt="배너" width={ 1920 } height={ 400 } className={'relative'}/>
+      {/*<Image src={ Banner } alt="배너" width={ 1920 } height={ 400 } className={'relative'}/>
       <div className={'flex absolute bottom-[400px]'}>
         <div className={'flex gap-5 '}>
           <Image src={ TopBannerImage01 } alt="수업사진" width={ 286 } height={ 362}  />
@@ -23,7 +24,25 @@ export default function TopBanner() {
           <p>협력하여 현대적이고 고급스러운 특수 페인트 및 금속</p>
           <p>마감 솔루션을 제공하고 있습니다.</p>
         </div>
+      </div>*/ }
+      <div className=" flex  justify-between w-[1920px] h-[420px] bg-gray-200 border-b border-neutral-200">
+        <div className={ "ml-[141px] mt-[93px]" }>
+          <div>
+            <p className={ "text-xl font-['EB\nGaramond']\n tracking-widest" }>EDUCATION INFO</p>
+            <p className={ "mt-[14px] text-6xl font-bold" }>교육 소개</p>
+          </div>
+          <div className="w-[529px] mt-[30px] text-black text-lg font-light font-['Helvetica'] leading-[30px]">
+            <p>창의적이고 특수한 기술을
+              활용하여 미술 작품이나 프로젝트를 창조하고자 하는 이들을 위한 훌륭한 기회를 제공합니다.
+              다양한 기법과 재료를 활용하여 특수한 효과와 아름다움을 창출하는 데 필요한 기술을 습득하고, 창의적인
+              작업을 펼칠 수 있습니다.</p>
+          </div>
+        </div>
+        <div>
+          <Image src={ Banner } alt={ "교육배너" } width={ 878 } height={ 420 } />
+        </div>
       </div>
     </section>
-  );
+  )
+    ;
 }
--- src/app/(education)/education/page.tsx
+++ src/app/(education)/education/page.tsx
@@ -5,17 +5,69 @@ import RouteComplete from "@/utils/RouteComplete";
 import { Edu } from "@/model/edu";
 import EducationCard from "@/app/(education)/education/EducationCard";
 import EduProcessingCard from "@/app/(education)/education/EduProcessingCard";
+import aplicatior from "@/assets/education/aplicator.jpg";
+import master from "@/assets/education/master.jpg";
+import oneday from "@/assets/education/oneday.jpg";
+import Image from "next/image";
 
 export default async function Page() {
   const edu: Edu[] = await getEdu();
-  // console.log(edu[0].eduName);
-  // console.log(edu[0].eduDescription);
   return (
     <RouteComplete>
-      <div>
-        <TopBanner />
-      </div>
-      <section className={ "w-[1308px] mx-auto mt-[81px]" }>
+      <TopBanner />
+      <section className={ "w-full h-[829px] flex " }>
+        <div className={'flex'}>
+          <div
+            className={ "flex flex-col mr-[40px] ml-[140px] mt-[218px] w-[276px] h-[319px] border-2 border-neutral-200 rounded-tl-2xl rounded-tr-2xl shadow-2xl" }>
+            <Image src={ aplicatior } alt={ "Aplicatior" } width={ 276 } height={ 320 } />
+            <div className={ "flex flex-col items-center w-[276px] h-[150px] bg-black rounded-bl-2xl rounded-br-2xl" }>
+              <p
+                className="text-white text-[33.94px] font-bold font-['Gmarket Sans'] mt-[25px] mb-[19px]">Aplicatior</p>
+              <p
+                className="w-[200px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight ">
+                실무에서 즉시 활용 가능한 역량을
+              </p>
+              <p
+                className={ "mb-[29px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight " }>강화할
+                수 있습니다.</p>
+            </div>
+          </div>
+          <div
+            className={ "flex flex-col ml-[140px] mt-[218px] w-[276px] h-[319px] border-2 border-neutral-200 rounded-tl-2xl rounded-tr-2xl shadow-2xl" }>
+            <Image src={ master } alt={ "Master" } width={ 276 } height={ 320 } />
+            <div className={ "flex flex-col items-center w-[276px] h-[150px] bg-black rounded-bl-2xl rounded-br-2xl" }>
+              <p
+                className="text-white text-[33.94px] font-bold font-['Gmarket Sans'] mt-[25px] mb-[19px]">Aplicatior</p>
+              <p
+                className="w-[200px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight ">
+                실무에서 즉시 활용 가능한 역량을
+              </p>
+              <p
+                className={ "mb-[29px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight " }>강화할
+                수 있습니다.</p>
+            </div>
+          </div>
+          <div
+            className={ "flex flex-col ml-[140px] mt-[218px] w-[276px] h-[319px] border-2 border-neutral-200 rounded-tl-2xl rounded-tr-2xl shadow-2xl" }>
+            <Image src={ oneday } alt={ "oneday" } width={ 276 } height={ 320 } />
+            <div className={ "flex flex-col items-center w-[276px] h-[150px] bg-black rounded-bl-2xl rounded-br-2xl" }>
+              <p
+                className="text-white text-[33.94px] font-bold font-['Gmarket Sans'] mt-[25px] mb-[19px]">Aplicatior</p>
+              <p
+                className="w-[200px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight ">
+                실무에서 즉시 활용 가능한 역량을
+              </p>
+              <p
+                className={ "mb-[29px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight " }>강화할
+                수 있습니다.</p>
+            </div>
+          </div>
+        </div>
+      
+      </section>
+      
+      
+      {/*<section className={ "w-[1308px] mx-auto mt-[81px]" }>
         <div>
           <p className={ "text-[24px]" }>EDUCATION INFO</p>
           <p className={ "text-[60px] font-bold" }>교육 소개</p>
@@ -34,14 +86,13 @@ export default async function Page() {
         <div className={ "divider mt-[116px]" } />
         <div className={ "mt-[48px]" } />
         <div className={ "flex justify-center flex-wrap w-[1200px] gap-[34px]" }>
-          {/*// sanity data map*/ }
           <EduProcessingCard />
           <EduProcessingCard />
           <EduProcessingCard />
           <EduProcessingCard />
           <EduProcessingCard />
         </div>
-      </section>
+      </section>*/ }
       {/*<EduProcessing />*/ }
     </RouteComplete>
   );
