79fd6f237d434c4af57d41707cad443be0e11af2
KimGyeongwon <ruddnjs6651@gmail.com>
2024-02-17 17:20:25 +0900

    상세설명 이미지 예외처리

--- src/app/(product)/product-details/[id]/DetailInfo.tsx
+++ src/app/(product)/product-details/[id]/DetailInfo.tsx
@@ -5,6 +5,12 @@ interface DetailInfoProps {
 }
 
 const DetailInfo = ({ detailImage }: DetailInfoProps) => {
+  if (!detailImage)
+    return (
+      <article className="overflow-auto w-[830px] h-screen bg-stone-300">
+        <p className="text-center p-10">상세 정보가 없습니다.</p>
+      </article>
+    );
   return (
     <article className="overflow-auto w-[830px]">
       <Image src={detailImage} alt="details" width={830} height={1080} />
