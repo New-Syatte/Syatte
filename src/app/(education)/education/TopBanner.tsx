1e89d98846f6e5b40a49f2d628c2ae506fae8b47
pelicanStd <pelican8118@gmail.com>
2024-02-18 23:15:53 +0900

    feat:edu section css

--- src/app/(education)/education/TopBanner.tsx
+++ src/app/(education)/education/TopBanner.tsx
@@ -6,29 +6,10 @@ import Banner from "@/assets/education/edubanner.jpg";
 export default function TopBanner() {
   return (
     <section>
-      {/*<Image src={ Banner } alt="배너" width={ 1920 } height={ 400 } className={'relative'}/>
-      <div className={'flex absolute bottom-[400px]'}>
-        <div className={'flex gap-5 '}>
-          <Image src={ TopBannerImage01 } alt="수업사진" width={ 286 } height={ 362}  />
-          <Image
-            src={ TopBannerImage02 }
-            alt="수업사진"
-            width={ 630 }
-            height={ 362}
-            className={'rounded-tr-3xl rounded-br-3xl'} />
-        </div>
-        <div className={'flex flex-col justify-center items-end ml-[129px]'}>
-          <p className={'text-[24px] mb-[15px]'}>EDUCATION</p>
-          <p className={'text-5xl font-bold mb-[38px]'}>포페인팅 교육</p>
-          <p className={'text-[18px]'}>샤뜨는 세계적인 특수 페인트 회사인 모던마스터와 마이더스 메탈과</p>
-          <p>협력하여 현대적이고 고급스러운 특수 페인트 및 금속</p>
-          <p>마감 솔루션을 제공하고 있습니다.</p>
-        </div>
-      </div>*/ }
       <div className=" flex  justify-between w-[1920px] h-[420px] bg-gray-200 border-b border-neutral-200">
         <div className={ "ml-[141px] mt-[93px]" }>
           <div>
-            <p className={ "text-xl font-['EB\nGaramond']\n tracking-widest" }>EDUCATION INFO</p>
+            <p className={ "text-xl font-['EB\nGaramond']\n tracking-widest" }>ACADEMY INFO</p>
             <p className={ "mt-[14px] text-6xl font-bold" }>교육 소개</p>
           </div>
           <div className="w-[529px] mt-[30px] text-black text-lg font-light font-['Helvetica'] leading-[30px]">
--- src/app/(education)/education/page.tsx
+++ src/app/(education)/education/page.tsx
@@ -5,95 +5,14 @@ import RouteComplete from "@/utils/RouteComplete";
 import { Edu } from "@/model/edu";
 import EducationCard from "@/app/(education)/education/EducationCard";
 import EduProcessingCard from "@/app/(education)/education/EduProcessingCard";
-import aplicatior from "@/assets/education/aplicator.jpg";
-import master from "@/assets/education/master.jpg";
-import oneday from "@/assets/education/oneday.jpg";
-import Image from "next/image";
+import EduSection from "@/app/(education)/education/EduSection";
 
 export default async function Page() {
   const edu: Edu[] = await getEdu();
   return (
     <RouteComplete>
       <TopBanner />
-      <section className={ "w-full h-[829px] flex " }>
-        <div className={'flex'}>
-          <div
-            className={ "flex flex-col mr-[40px] ml-[140px] mt-[218px] w-[276px] h-[319px] border-2 border-neutral-200 rounded-tl-2xl rounded-tr-2xl shadow-2xl" }>
-            <Image src={ aplicatior } alt={ "Aplicatior" } width={ 276 } height={ 320 } />
-            <div className={ "flex flex-col items-center w-[276px] h-[150px] bg-black rounded-bl-2xl rounded-br-2xl" }>
-              <p
-                className="text-white text-[33.94px] font-bold font-['Gmarket Sans'] mt-[25px] mb-[19px]">Aplicatior</p>
-              <p
-                className="w-[200px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight ">
-                실무에서 즉시 활용 가능한 역량을
-              </p>
-              <p
-                className={ "mb-[29px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight " }>강화할
-                수 있습니다.</p>
-            </div>
-          </div>
-          <div
-            className={ "flex flex-col ml-[140px] mt-[218px] w-[276px] h-[319px] border-2 border-neutral-200 rounded-tl-2xl rounded-tr-2xl shadow-2xl" }>
-            <Image src={ master } alt={ "Master" } width={ 276 } height={ 320 } />
-            <div className={ "flex flex-col items-center w-[276px] h-[150px] bg-black rounded-bl-2xl rounded-br-2xl" }>
-              <p
-                className="text-white text-[33.94px] font-bold font-['Gmarket Sans'] mt-[25px] mb-[19px]">Aplicatior</p>
-              <p
-                className="w-[200px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight ">
-                실무에서 즉시 활용 가능한 역량을
-              </p>
-              <p
-                className={ "mb-[29px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight " }>강화할
-                수 있습니다.</p>
-            </div>
-          </div>
-          <div
-            className={ "flex flex-col ml-[140px] mt-[218px] w-[276px] h-[319px] border-2 border-neutral-200 rounded-tl-2xl rounded-tr-2xl shadow-2xl" }>
-            <Image src={ oneday } alt={ "oneday" } width={ 276 } height={ 320 } />
-            <div className={ "flex flex-col items-center w-[276px] h-[150px] bg-black rounded-bl-2xl rounded-br-2xl" }>
-              <p
-                className="text-white text-[33.94px] font-bold font-['Gmarket Sans'] mt-[25px] mb-[19px]">Aplicatior</p>
-              <p
-                className="w-[200px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight ">
-                실무에서 즉시 활용 가능한 역량을
-              </p>
-              <p
-                className={ "mb-[29px] text-white text-sm font-normal font-['Helvetica'] uppercase leading-tight " }>강화할
-                수 있습니다.</p>
-            </div>
-          </div>
-        </div>
-      
-      </section>
-      
-      
-      {/*<section className={ "w-[1308px] mx-auto mt-[81px]" }>
-        <div>
-          <p className={ "text-[24px]" }>EDUCATION INFO</p>
-          <p className={ "text-[60px] font-bold" }>교육 소개</p>
-        </div>
-        <div className={ "flex flex-wrap gap-[24px] mb-[205px]" }>
-          { edu.map((item: Edu) => (
-            <div key={ item._id }>
-              <EducationCard eduName={ item.eduName } eduDescription={ item.eduDescription } />
-            </div>
-          )) }
-        </div>
-      </section>
-      <div className={ "" } />
-      <section className={ "mx-auto w-[70vw] " }>
-        <p className={ "text-[60px] font-bold text-center" }>진행중인 교육</p>
-        <div className={ "divider mt-[116px]" } />
-        <div className={ "mt-[48px]" } />
-        <div className={ "flex justify-center flex-wrap w-[1200px] gap-[34px]" }>
-          <EduProcessingCard />
-          <EduProcessingCard />
-          <EduProcessingCard />
-          <EduProcessingCard />
-          <EduProcessingCard />
-        </div>
-      </section>*/ }
-      {/*<EduProcessing />*/ }
+      <EduSection />
     </RouteComplete>
   );
 }
