ec54008bb250c16ba11e9d93e55da4de5eae76bf
DICEPT <dicepted@gmail.com>
2024-02-18 20:47:09 +0900

    Feat:  상호명 변경

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -4,12 +4,12 @@ export default function Footer() {
   return (
     <footer className={styles.container}>
       <div className={styles.footer}>
-        <div className="font-semibold mb-[10px]">Columbia Tools</div>
-        <div className="mb-[10px]">주소 기입</div>
+        <div className="font-semibold mb-[10px]">SYATT</div>
+        <div className="mb-[10px]">서울시 은평구 서오릉로 149-26(구산동 2-25번지) 1층</div>
         <div className="flex mb-[30px]">
           <div className="font-semibold">고객센터</div>
           <div className="font-normal ml-[19px]">
-            1566-1000 ㅣ AM 09:00 - PM 06:00
+          02-359-7185 ㅣ AM 09:00 - PM 06:00
           </div>
         </div>
         <div className="mb-[10px]">
@@ -17,7 +17,7 @@ export default function Footer() {
           신고번호:신고번호ㅣ사업자 등록번호:156-66-100004
         </div>
         <div className="mb-[10px]">개인정보 처리방침ㅣ서비스 이용약관</div>
-        <div>Copyright (c) 2023 Columbia Tools. All rights reserved.</div>
+        <div>Copyright (c) 2023 SYATT. All rights reserved.</div>
       </div>
     </footer>
   );
