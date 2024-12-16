b41c4bfcf41a270d89f30f3cedd9ec72dbcb6901
KimGyeongwon <ruddnjs6651@gmail.com>
2024-12-16 22:27:39 +0900

    fix : vercel build 오류 : next 15 migrate에 따른 searchParams 수정

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,29 +1,31 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { getProviders } from "next-auth/react";
-import Signin from "@/components/Login/Signin";
+import SignInClient from "@/components/Login/Signin";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-type Props = {
+interface PageProps {
   searchParams: {
-    callbackUrl: string;
+    callbackUrl?: string;
   };
-};
+}
 
-export default async function SignInPage({
-  searchParams: { callbackUrl },
-}: Props) {
-  const session = await getServerSession(authOptions); // 서버에서 session 정보 호출
+export default async function SignInPage({ searchParams }: PageProps) {
+  const session = await getServerSession(authOptions);
+  const providers = await getProviders();
+  const callbackUrl = searchParams?.callbackUrl || "/";
 
   if (session) {
+    redirect(callbackUrl);
+  }
+
+  if (!providers) {
     redirect("/");
   }
 
-  const providers = (await getProviders()) ?? redirect("/");
-
   return (
-    <section className="flex justify-center pt-20 pb-52">
-      <Signin providers={providers} callbackUrl={callbackUrl ?? "/"} />
+    <section className="w-full h-screen flex justify-center items-center">
+      <SignInClient providers={providers} callbackUrl={callbackUrl} />
     </section>
   );
 }
